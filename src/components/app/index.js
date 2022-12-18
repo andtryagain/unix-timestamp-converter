ccce78d5e85a1e1bdd1a0efb4c047e4b8fc04a72
andtryagain <iliabondarev@protonmail.com>
2022-12-18 18:50:30 +0400

    package.json and minimal script

---
+++ src/components/app/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const App = () => {
+    return (
+        <div className='mt-5 ms-5'>
+            <h2>unix-timestamp-converter</h2>
+        </div>
+    );
+}
+
+export default App;
